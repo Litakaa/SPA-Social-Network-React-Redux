87317af46e32bfb73b223fcce5e485abfd678643
Litakaa <lita.ju@yandex.ru>
2022-10-06 15:26:19 +0300

    test for ProfileStatus Component (react-test-renderer)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -9,10 +9,11 @@ import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
-import {connect} from "react-redux";
+import {connect, Provider} from "react-redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import Navbar from "./components/Navbar/Navbar";
+import store from "./redux/redux-store";
 
 
 class App extends React.Component {
@@ -24,23 +25,23 @@ class App extends React.Component {
             return <Preloader/>
         }
         return (
-                    <div className='app-wrapper'>
-                        <HeaderContainer/>
-                        <Navbar/>
-                        <div className='app-wrapper-content'>
-                            <Routes>
-                                <Route path="/dialogs/*" element={<DialogsContainer/>}/>
-                                <Route path="/profile" element={<ProfileContainer/>}>
-                                    <Route path=":userId" element={<ProfileContainer/>}/>
-                                </Route>
-                                <Route path="/users" element={<UsersContainer/>}/>
-                                <Route path="/news/*" element={<News/>}/>
-                                <Route path="/music" element={<Music/>}/>
-                                <Route path="/settings" element={<Settings/>}/>
-                                <Route path="/login" element={<Login/>}/>
-                            </Routes>
-                        </div>
+                <div className='app-wrapper'>
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className='app-wrapper-content'>
+                        <Routes>
+                            <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+                            <Route path="/profile" element={<ProfileContainer/>}>
+                                <Route path=":userId" element={<ProfileContainer/>}/>
+                            </Route>
+                            <Route path="/users" element={<UsersContainer/>}/>
+                            <Route path="/news/*" element={<News/>}/>
+                            <Route path="/music" element={<Music/>}/>
+                            <Route path="/settings" element={<Settings/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                        </Routes>
                     </div>
+                </div>
         );
     }
 }
@@ -50,4 +51,13 @@ const mapStateToProps = (state) => {
         initialized: state.app.initialized
     }
 }
-export default connect(mapStateToProps, {initializeApp})(App);
+const AppContainer =  connect(mapStateToProps, {initializeApp})(App);
+
+const MainApp = (props) => {
+    return <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </BrowserRouter>
+}
+export default MainApp;
--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import store from "./redux/redux-store";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import MainApp from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </BrowserRouter>);
+root.render(<MainApp/>);
 
 
 // If you want to start measuring performance in your app, pass a function
