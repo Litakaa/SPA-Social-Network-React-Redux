672155a19e4d27516fceefdb7a8e31b2967b24cd
Litakaa <lita.ju@yandex.ru>
2022-10-19 10:22:19 +0300

    import

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,13 +1,13 @@
 import {combineReducers} from "redux";
 import { createStore, applyMiddleware } from 'redux';
-import profileReducer from "./profile-reducer";
-import dialogsReducer from "./dialogs-reducer";
-import usersReducer from "./users-reducer";
-import authReducer from "./auth-reducer";
+import profileReducer from "./profile-reducer.ts";
+import dialogsReducer from "./dialogs-reducer.ts";
+import usersReducer from "./users-reducer.ts";
+import authReducer from "./auth-reducer.ts";
 import thunkMiddleware from "redux-thunk";
 import { reducer as formReducer } from 'redux-form';
 import appReducer from "./app-reducer";
-import sideBarReducer from "./sideBar-reducer";
+import sideBarReducer from "./sideBar-reducer.ts";
 
 let reducer = combineReducers({
     profilePage: profileReducer,
